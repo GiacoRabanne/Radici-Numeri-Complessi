79e4fe0d11db19f8adcabf00e8c29f230314e26d
Giacomo Radicchi <161774747+GiacoRabanne@users.noreply.github.com>
2024-02-29 23:39:05 +0100

    Update Complesso.java

--- calcolaRadice/Complesso.java
+++ calcolaRadice/Complesso.java
@@ -5,20 +5,22 @@ public class Complesso {
     double immaginario;
 
     static public Complesso calcolaRadice(Complesso termine, int n, int i) {
-        double modulo = Math.sqrt(termine.reale*termine.reale + termine.immaginario*termine.immaginario);
-        double r = Math.pow(modulo, (double) 1/n);
-        double radianti = Math.acos(termine.reale/modulo);
+        double modulo = Math.sqrt(termine.reale*termine.reale + termine.immaginario*termine.immaginario); //il modulo di un vettore lo calcolo con pitagora
+        double r = Math.pow(modulo, (double) 1/n); //ottengo il valore di r, facendo una semplice radice n-esima di un numero positivo.
+        double radianti = Math.acos(termine.reale/modulo); // termine_reale = modulo * cos(angolo) -> angolo = arcocoseno(termine_reale/modulo)
         Complesso radice = new Complesso();
 
-        double coseno = Math.cos(radianti/n + i*2*Math.PI/n);
+        double coseno = Math.cos(radianti/n + i*2*Math.PI/n); //i rappresenta la classe di resto i in modulo n (è un valore che va da 0 a n-1)
         double seno = Math.sin(radianti/n + i*2*Math.PI/n);
-        radice.reale = r * coseno;
+        radice.reale = r * coseno; 
         radice.immaginario = r * seno;
 
-        if(Math.abs(Math.abs(radice.reale) - Math.abs(Math.round(radice.reale))) < Double.MIN_VALUE) {
+        //la parte che segue serve solo per arrotondare il valore ottenuto.
+
+        if(Math.abs(Math.abs(radice.reale) - Math.abs(Math.round(radice.reale))) < 0.001) {
             radice.reale = Math.round(radice.reale);
         }
-        if(Math.abs(Math.abs(radice.immaginario) - Math.abs(Math.round(radice.immaginario))) < Double.MIN_VALUE) {
+        if(Math.abs(Math.abs(radice.immaginario) - Math.abs(Math.round(radice.immaginario))) < 0.001) {
             radice.immaginario = Math.round(radice.immaginario);
         } 
 
