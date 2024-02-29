f98b4278108b4a1d6c58403b451875ae1ee303b0
Giacomo Radicchi <161774747+GiacoRabanne@users.noreply.github.com>
2024-02-29 23:52:10 +0100

    Update Radici.java

--- calcolaRadice/Radici.java
+++ calcolaRadice/Radici.java
@@ -8,7 +8,6 @@ public class Radici {
         radicando.reale = in.readDouble("Inserisci la parte reale del radicando:");
         radicando.immaginario = in.readDouble("Inserisci la parte complessa del radicando:");
         int n = in.readInt("Inserisci il grado della radice da calcolare");
-        Complesso radice = new Complesso();
         OutputWindow out = new OutputWindow("Radici Calcolate");
         out.setFont("SF Pro Display", 20);
         out.writeln("Radici calcolate:\n");
@@ -16,20 +15,20 @@ public class Radici {
         for(int i = 0; i < n; i++) {
             out.write("X"+ (i+1) + " = ");
             boolean trovato = false;
-            if(Math.abs(radice.calcolaRadice(radicando, n, i).reale) > 0.001){
-                out.write(radice.calcolaRadice(radicando, n, i).reale);
+            if(Math.abs(radicando.calcolaRadice(n, i).reale) > 0.001){
+                out.write(radicando.calcolaRadice(n, i).reale);
                 trovato = true;
             }
 
-            if(Math.abs(radice.calcolaRadice(radicando, n, i).immaginario) > 0.001){
-                if(radice.calcolaRadice(radicando, n, i).immaginario < 0) {
+            if(Math.abs(radicando.calcolaRadice(n, i).immaginario) > 0.001){
+                if(radicando.calcolaRadice(n, i).immaginario < 0) {
                     out.write(" -");
                 } else if(trovato) {
                     out.write(" +");
                 }
                 out.write("i");
-                if(Math.abs(radice.calcolaRadice(radicando, n, i).immaginario) != 1.0 ) {
-                    out.write("*(" + Math.abs(radice.calcolaRadice(radicando, n, i).immaginario) + ")");
+                if(Math.abs(radicando.calcolaRadice(n, i).immaginario) != 1.0 ) {
+                    out.write("*(" + Math.abs(radicando.calcolaRadice(n, i).immaginario) + ")");
                 }
                 
                 trovato = true;
